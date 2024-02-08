6a4f2ed4b3ab2df5d6d44aad7bdb4983947f381e
Jan Beelte <jan.beelte@mailbox.org>
2024-02-08 14:46:00 +0100

    (add) simplify for geojson.

--- geojson_export/geojson_export/main.py
+++ geojson_export/geojson_export/main.py
@@ -2,7 +2,6 @@ import argparse
 import glob
 import logging
 import os
-import tempfile
 
 import geobuf
 import geopandas as gpd
@@ -18,6 +17,7 @@ if __name__ == '__main__':
     parser.add_argument('--output-folder', default='../data/segmentation-ir')
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-c', '--compress', action='store_true')
+    parser.add_argument('-s', '--simplify', action='store_true')
 
     args = parser.parse_args()
     
@@ -29,11 +29,14 @@ if __name__ == '__main__':
     assert os.path.exists(args.input_folder), f"Input folder not found: {args.input_folder}"
     gpkg_files = glob.glob(os.path.join(args.input_folder, "**/*.gpkg"), recursive=True)
     assert len(gpkg_files) > 0, f"No .gpkg files found in {args.input_folder}"
+    os.makedirs(args.output_folder, exist_ok=True)
     for input_filepath in gpkg_files:
         log.info(f"Processing {input_filepath} ...")
         basename = os.path.basename(os.path.splitext(input_filepath)[0])
         output_filepath = os.path.join(args.output_folder, basename)
         gdf = gpd.read_file(filename=input_filepath)
+        if args.simplify:
+            gdf.simplify(0.2, preserve_topology=False)
         if args.compress:
             geo_json = gdf.to_json()
             with open(f"{output_filepath}.pbf", 'w') as f:
